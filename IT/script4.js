d1dc996ca03d1a1c1c27d93fd3b2926a3b358f96
Ajay <email.ajaysinghroha@gmail.com>
2019-07-29 12:09:50 +0530

    Added js prgrams

--- IT/script4.js
+++ IT/script4.js
@@ -1,1 +1,8 @@
-var selector = ""
+
+function onCLick(){
+
+    alert(document.forms[1].children[0].value);
+    alert("has Children? "+document.forms[1].children[0].hasChildNodes());
+    alert("has siblings? "+document.forms[1].children[0].hasSiblings());
+
+}
