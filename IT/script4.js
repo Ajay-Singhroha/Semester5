ffaf90e072ac3d680e9e35afe10d8d0faf0d550b
Ajay <email.ajaysinghroha@gmail.com>
2019-09-02 00:55:43 +0530

    Delete script4.js

--- IT/script4.js
+++
@@ -1,8 +0,0 @@
-
-function onCLick(){
-
-    alert(document.forms[1].children[0].value);
-    alert("has Children? "+document.forms[1].children[0].hasChildNodes());
-    alert("has siblings? "+document.forms[1].children[0].hasSiblings());
-
-}
