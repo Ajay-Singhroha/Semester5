f2b8d5e048f235585ce3e7c6ab55a09f49ce6b5a
Ajay <email.ajaysinghroha@gmail.com>
2019-09-02 00:55:51 +0530

    Delete script3.js

--- IT/script3.js
+++
@@ -1,8 +0,0 @@
-function auth(){
-    if(document.getElementById('password').value=="ajayajay"){
-        alert(" Hey, "+ document.getElementById('id').value+" You are welcome!");
-    }
-    else{
-        alert(" Access Denied");
-    }
-}
